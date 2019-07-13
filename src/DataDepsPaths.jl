817db142b4fd7061b957450482b61c4c81b509cb
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-07-13 14:12:23 -0500

    Make package load and fix registration
    Fixed struct definition order
    Added DataDepPathRegistration per README example
    Fixed DataDeps function imports
    Misc. whitespace cleanup

--- src/DataDepsPaths.jl
+++ src/DataDepsPaths.jl
@@ -4,21 +4,29 @@ using DataDeps: register, AbstractDataDep
 using FilePathsBase
 
 
+struct Satisfier
+    satisfies # DataDepPath → Bool
+    resolve # () → AbstractPath
+end
+Satisfier((satisfies, resolve)) =
+    Satisfier(satisfies, resolve)
+
 struct DataDependency <: AbstractDataDep
     name::String
     message::String
     satisfiers::Vector{Satisfier}
 end
 
-struct Satisfier
-    satisfies # DataDepPath → Bool
-    resolve # () → AbstractPath
-end
-
 struct DataDepPath # Should we actually subtype AbstractPath here?
     name::String
     parts::Vector{String}
 end
+
+struct DataDepPathRegistration
+    satisfiers::Vector{Satisfier}
+end
+DataDepPathRegistration(satisfiers...) =
+    DataDepPathRegistration(collect(Satisfier.(satisfiers)))
 
 function satisfy(datadep::DataDependency, dd_path::DataDepPath)
     @assert(datadep.name == dd_path.name)
